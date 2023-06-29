55d87e88b648cc77ccaf39d56d8801d4969b12d3
mhiztaRIZ <116142895+mhiztaRIZ@users.noreply.github.com>
2023-06-29 15:04:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var translateButton = document.getElementById("translateButton");
-    translateButton.addEventListener("click", translateText);
+document.getElementById("translateBtn").addEventListener("click", function() {
+  const sourceText = document.getElementById("sourceText").value;
+  const targetLanguage = document.getElementById("targetLanguage").value;
+
+  translateText(sourceText, targetLanguage)
+    .then(function(translationResult) {
+      document.getElementById("translationResult").innerText = translationResult;
+    })
+    .catch(function(error) {
+      console.error("Translation error:", error);
+    });
 });
 
-function translateText() {
-    var sourceText = document.getElementById("sourceText").value;
+function translateText(text, targetLanguage) {
+  // Replace 'YOUR_API_KEY' with your actual API key
+  const apiKey = 'YOUR_API_KEY';
+  const endpoint = `https://translation-api.example.com/translate?apiKey=${apiKey}`;
 
-    if (sourceText !== "") {
-        var xhr = new XMLHttpRequest();
-        var url = "translate.php";
-        var params = "text=" + encodeURIComponent(sourceText);
+  const requestData = {
+    text: text,
+    targetLanguage: targetLanguage
+  };
 
-        xhr.open("POST", url, true);
-        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-                var response = JSON.parse(xhr.responseText);
-                displayTranslation(response.translation);
-            }
-        };
-
-        xhr.send(params);
-    }
-}
-
-function displayTranslation(translation) {
-    var translationResult = document.getElementById("translationResult");
-    translationResult.innerText = "Translation: " + translation;
+  return fetch(endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(requestData)
+  })
+    .then(function(response) {
+      if (!response.ok) {
+        throw new Error("Translation request failed.");
+      }
+      return response.json();
+    })
+    .then(function(data) {
+      return data.translation;
+    });
 }
